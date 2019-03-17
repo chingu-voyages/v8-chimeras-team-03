5950bf4e2076beefde13fcaab32b6869b923b54f
Piotr Grzybowski <grzybek6@gmail.com>
2019-03-17 19:44:20 +0000

    Added folders structure and readme.md files in each of the directories with guidelines what should go inside every folder

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send('Welcome to Express :)');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+});
