f0130dca31d71e6012a2360bbc364a264cc545e8
bikilone <milosbirovljevic@gmail.com>
2019-03-28 21:10:12 +0100

    added video

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import "./LandingPage.scss";
+import video from "../../assets/video.mp4";
 
-const LandingPage = props => {
-  return <div>Landing Page</div>;
-};
-
-export default LandingPage;
+export default class LandingPage extends Component {
+  componentDidMount() {
+    this.refs.video.play();
+  }
+  render() {
+    return (
+      <div className="landing-page" ref={this.landing}>
+        <h1>Where did time go?</h1>
+        <p>Turn your team on to productivity with Toggl teh time tracker.</p>
+        <video ref="video" src={video} />
+        <button>
+          SIGN UP <span />
+        </button>
+        <div className="background-pink" ref="absolute" />
+      </div>
+    );
+  }
+}
