1656fd0c2c096e2c8fb18fa924470b1b503e34d8
bikilone <milosbirovljevic@gmail.com>
2019-05-01 22:22:33 +0200

    packed the task data into an object

--- src/containers/DashboardPage/DashboardPage.js
+++ src/containers/DashboardPage/DashboardPage.js
@@ -123,16 +123,74 @@ class DashboardPage extends Component {
     const { onTimerClick, onInputChange } = this;
     const { hours, minutes, seconds } = timeParser(this.state.timer);
     const tasks = [];
+
+    // task array contains list of all tasks
+    // each task is an object
+    // task: {
+    //   singleTask: true/false // has this task been used multiple times
+    //   times: [
+    //     {
+    //       startTime: num,
+    //       endTime: num,
+    //       timeDif: num
+    //     },
+    //     {
+    //       startTime: num,
+    //       endTime: num,
+    //       timeDif: num
+    //     }
+    //   ],
+    //   taskName: name,
+    //   sumTimeDifference: timeDif, // sums all time differences
+    //
+    // }
+
     if (Object.keys(listofTasks).length > 0) {
       var x;
       for (x in listofTasks) {
-        tasks.push([
-          listofTasks[x].taskName,
-          listofTasks[x].startTime,
-          listofTasks[x].endTime
-        ]);
+        const index = tasks.findIndex(element => {
+          return element.taskName === listofTasks[x].taskName;
+        });
+        if (index !== -1) {
+          // there is already a task with the same name
+          tasks[index] = {
+            taksName: tasks[index].taskName,
+            singleTask: false,
+            times: [
+              ...tasks[index].times,
+              {
+                startTime: listofTasks[x].startTime,
+                endTime: listofTasks[x].endTime,
+                timeDif: listofTasks[x].endTime - listofTasks[x].startTime
+              }
+            ],
+            sumTimDif: tasks[index].times.reduce(
+              (accumulator, currentValue) => {
+                return accumulator + currentValue.timeDif;
+              },
+              0
+            )
+          };
+        } else {
+          // there is no task with the same name
+          tasks.push({
+            taskName: listofTasks[x].taskName,
+            singleTask: true,
+            times: [
+              {
+                startTime: listofTasks[x].startTime,
+                endTime: listofTasks[x].endTime,
+                timeDif: listofTasks[x].endTime - listofTasks[x].startTime
+              }
+            ]
+          });
+          tasks[tasks.length - 1].sumTimDif =
+            tasks[tasks.length - 1].times[0].timeDif;
+        }
       }
+      console.log(tasks);
     }
+
     return (
       <div className="dashboard">
         <div className="menu">
@@ -169,7 +227,7 @@ class DashboardPage extends Component {
           <div className="taskList">
             <ul>
               {/* {tasks.length>0?tasks:""} */}
-              {tasks.length > 0
+              {/* {tasks.length > 0
                 ? tasks.map((task, i) => (
                     <li key={i}>
                       <div className="task">
@@ -183,7 +241,7 @@ class DashboardPage extends Component {
                       </div>
                     </li>
                   ))
-                : ""}
+                : ""} */}
             </ul>
           </div>
         </div>
